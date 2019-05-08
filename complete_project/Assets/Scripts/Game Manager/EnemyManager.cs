c513d6c22778fb91805c97ce0f9d073c4e1fa964
David Pickett <pickettd@gmail.com>
2019-05-07 14:57:32 -1000

    Make sure zombie naming is working in complete project

--- complete_project/Assets/Scripts/Game Manager/EnemyManager.cs
+++ complete_project/Assets/Scripts/Game Manager/EnemyManager.cs
@@ -7,14 +7,14 @@ public class EnemyManager : MonoBehaviour {
     public static EnemyManager instance;
 
     [SerializeField]
-    private GameObject boar_Prefab, cannibal_Prefab;
+    private GameObject boar_Prefab, zombie_Prefab;
 
-    public Transform[] cannibal_SpawnPoints, boar_SpawnPoints;
+    public Transform[] zombie_SpawnPoints, boar_SpawnPoints;
 
     [SerializeField]
-    private int cannibal_Enemy_Count, boar_Enemy_Count;
+    private int zombie_Enemy_Count, boar_Enemy_Count;
 
-    private int initial_Cannibal_Count, initial_Boar_Count;
+    private int initial_Zombie_Count, initial_Boar_Count;
 
     public float wait_Before_Spawn_Enemies_Time = 10f;
 
@@ -24,7 +24,7 @@ public class EnemyManager : MonoBehaviour {
 	}
 
     void Start() {
-        initial_Cannibal_Count = cannibal_Enemy_Count;
+        initial_Zombie_Count = zombie_Enemy_Count;
         initial_Boar_Count = boar_Enemy_Count;
 
         SpawnEnemies();
@@ -39,27 +39,27 @@ public class EnemyManager : MonoBehaviour {
     }
 
     void SpawnEnemies() {
-        SpawnCannibals();
+        SpawnZombies();
         SpawnBoars();
     }
 
-    void SpawnCannibals() {
+    void SpawnZombies() {
 
         int index = 0;
 
-        for (int i = 0; i < cannibal_Enemy_Count; i++) {
+        for (int i = 0; i < zombie_Enemy_Count; i++) {
 
-            if (index >= cannibal_SpawnPoints.Length) {
+            if (index >= zombie_SpawnPoints.Length) {
                 index = 0;
             }
 
-            Instantiate(cannibal_Prefab, cannibal_SpawnPoints[index].position, Quaternion.identity);
+            Instantiate(zombie_Prefab, zombie_SpawnPoints[index].position, Quaternion.identity);
 
             index++;
 
         }
 
-        cannibal_Enemy_Count = 0;
+        zombie_Enemy_Count = 0;
 
     }
 
@@ -87,7 +87,7 @@ public class EnemyManager : MonoBehaviour {
     IEnumerator CheckToSpawnEnemies() {
         yield return new WaitForSeconds(wait_Before_Spawn_Enemies_Time);
 
-        SpawnCannibals();
+        SpawnZombies();
 
         SpawnBoars();
 
@@ -95,14 +95,14 @@ public class EnemyManager : MonoBehaviour {
 
     }
 
-    public void EnemyDied(bool cannibal) {
+    public void EnemyDied(bool zombie) {
 
-        if(cannibal) {
+        if(zombie) {
 
-            cannibal_Enemy_Count++;
+            zombie_Enemy_Count++;
 
-            if(cannibal_Enemy_Count > initial_Cannibal_Count) {
-                cannibal_Enemy_Count = initial_Cannibal_Count;
+            if(zombie_Enemy_Count > initial_Zombie_Count) {
+                zombie_Enemy_Count = initial_Zombie_Count;
             }
 
         } else {
